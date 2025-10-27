305450de27aa94a81975cfb083d8c9e8a7d5d433
Robin <robinnem@outlook.com>
2025-10-27 16:50:44 -0400

    Finished Website, Added Responsiveness and Shape Animations

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const booleanInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * booleanInt}px, ${
+      y * booleanInt
+    }px)`;
+  }
+}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
